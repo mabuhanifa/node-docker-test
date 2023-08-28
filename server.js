b686a773de8b5e37c2fbfc361a16dd3bad96373f
mabuhanifa <moh.abuhanifa@gmail.com>
2023-08-28 18:36:44 +0600

    dockerfile added

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
